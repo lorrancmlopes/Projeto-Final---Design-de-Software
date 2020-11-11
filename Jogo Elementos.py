77acc753bdffef490d15a1aba8b6903df0d3d9f1
LidiaDomingos <lidiaalves2002@outlook.com>
2020-11-11 08:39:56 -0300

    pra tela não fechar

--- Jogo Elementos.py
+++ Jogo Elementos.py
@@ -9,3 +9,14 @@ WIDTH = 1377
 HEIGHT = 720
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Lord Element')
+
+game = True
+
+while game:
+    for event in pygame.event.get():
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+            game = False
+
+
+
