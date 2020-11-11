d14b9f6c73b25ce2872cf6554bf6f28bfc488ba6
LidiaDomingos <lidiaalves2002@outlook.com>
2020-11-11 08:26:32 -0300

    criando tela

--- Jogo Elementos.py
+++ Jogo Elementos.py
@@ -1,4 +1,11 @@
-print('hello')
-print('oioioioi')
-print('lidia')
-print("lorran")
+import pygame
+import random
+import time
+
+pygame.init()
+
+#gera tela
+WIDTH = 1377
+HEIGHT = 720
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Lord Element')
