ea06b40906c283b0929f07f78a062a5ff79f20f9
Lorran Lopes <lorranlopes13@hotmail.com>
2020-11-11 09:11:39 -0300

    Inserção do background

--- Jogo Elementos.py
+++ Jogo Elementos.py
@@ -5,10 +5,13 @@ import time
 pygame.init()
 
 #gera tela
-WIDTH = 1377
-HEIGHT = 720
+WIDTH = 956
+HEIGHT = 510
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Lord's Element")
+image = pygame.image.load('fundo1.png').convert()
+image = pygame.transform.scale(image, (956, 510))
+
 
 game = True
 
@@ -18,5 +21,12 @@ while game:
         if event.type == pygame.QUIT:
             game = False
 
+    window.fill((0, 0, 0))  # Preenche com a cor branca
+    window.blit(image, (0, 0))
 
+    # ----- Atualiza estado do jogo
+    pygame.display.update()  # Mostra o novo frame para o jogador
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
 
